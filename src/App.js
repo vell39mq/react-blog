532a9a5fc5bb4160587212d675e6758a920560f5
vell39mq <51503089+vell39mq@users.noreply.github.com>
2022-07-09 09:43:16 +0900

    react-router-dom@6をインストールしてルーティングを設定 ナビゲーションを作成し、cssを当てた

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router,Routes,Route,Link} from "react-router-dom"
+import Home from "./pages/Home"
+import Login from './pages/Login';
+import CreatePost from './pages/CreatePost';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/createpost">Create Post</Link>
+        <Link to="/login">Login</Link>
+      </nav>
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/createpost" element={<CreatePost />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='loginPage'>
+      <p>Sign in with Google to continue</p>
+      <button className='login-with-google-btn'>
+        Sign in with Google
+      </button>
+    </div>
+  )
+}
+
+export default Login
