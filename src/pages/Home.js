40440249ded4beef85413477b908b3bb17ec9600
vell39mq <51503089+vell39mq@users.noreply.github.com>
2022-07-09 20:12:06 +0900

    ホーム画面で投稿を見れるように実装

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import { collection, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { db } from "../firebase-config";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [postLists, setPostLists] = useState([]);
+  const postCollectionRef = collection(db, "posts");
 
-export default Home
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postCollectionRef);
+      setPostLists(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getPosts();
+  });
+
+  return (
+    <div className="homePage">
+      {postLists.map((post) => {
+        return (
+          <div className="post">
+            <div className="postHeader">
+              <div className="title">
+                <h1>{post.title}</h1>
+              </div>
+              <div className="deletePost">
+                <button>&#x1f5d1;</button>
+              </div>
+            </div>
+            <div className="postTextContainer">{post.postText}</div>
+            <h3>{post.author.name}</h3>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Home;
